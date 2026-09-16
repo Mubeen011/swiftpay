a83088283c7721ff2a66bbf18904d1cc8ae0223d
Mubeen Ahmed <mohammadmubeenahmed01@gmail.com>
2026-09-16 16:48:08 +0530

    Fix ledger CI test database

--- ledger-service/src/test/java/com/swiftpay/ledgerservice/LedgerServiceApplicationTests.java
+++ ledger-service/src/test/java/com/swiftpay/ledgerservice/LedgerServiceApplicationTests.java
@@ -2,12 +2,30 @@ package com.swiftpay.ledgerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+                "spring.kafka.listener.auto-startup=false",
+                "spring.datasource.url=jdbc:h2:mem:swiftpaytest",
+                "spring.datasource.driver-class-name=org.h2.Driver",
+                "spring.datasource.username=sa",
+                "spring.datasource.password=",
+                "spring.jpa.hibernate.ddl-auto=create-drop",
+                "spring.sql.init.mode=never"
+        }
+)
 class LedgerServiceApplicationTests {
+
+    @MockitoBean
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @MockitoBean
+    private KafkaAdmin kafkaAdmin;
 
     @Test
     void contextLoads() {
     }
-
 }
